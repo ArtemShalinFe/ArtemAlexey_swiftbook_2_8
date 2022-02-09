595df03b3a25860246b87bc06b5cee7d0a133bfa
Artem Shalin <a.shalin@rtinform.ru>
2022-02-09 21:52:03 +0300

    Initial Commit

---
+++ ArtemAlexey_swiftbook_2_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtemAlexey_swiftbook_2_8
+//
+//  Created by Артем ШАЛИН on 09.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
